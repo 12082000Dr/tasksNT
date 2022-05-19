d9b26e0a683d3616ac7a5218ab83feae57b299ce
Danila Rogozhnikov <100974943+12082000Dr@users.noreply.github.com>
2022-05-19 08:26:16 +0400

    Update Task2.py

--- task2/Task2.py
+++ task2/Task2.py
@@ -14,12 +14,16 @@ points = [i.split(' ') for i in points]
 centre = [float(centre_rad[0][0]), float(centre_rad[0][1])]
 rad = float(centre_rad[1][0])
 
+result = []
+
 for i, j in points:
     point = [float(i), float(j)]
     dist = distance(point, centre)
     if dist == rad:
-        print(0)
+        result.append(0)
     elif dist < rad:
-        print(1)
+        result.append(1)
     elif dist > rad:
-        print(2)
+        result.append(2)
+        
+print(*result, sep='\n')
